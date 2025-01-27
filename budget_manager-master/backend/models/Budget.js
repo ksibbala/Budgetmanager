0c81531cbe7f9b781b568e85d660599482c60be8
Kavya Priya Sibbala <134634035+ksibbala@users.noreply.github.com>
2025-01-27 16:27:21 -0700

    Delete budget_manager-master directory

--- budget_manager-master/backend/models/Budget.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const budgetSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  totalBudget: { type: Number, required: true },
-  savingsGoal: { type: Number },
-}, { timestamps: true });
-
-module.exports = mongoose.model('Budget', budgetSchema);
--- budget_manager-master/frontend/src/services/apiService.js
+++
@@ -1,8 +0,0 @@
-// src/services/apiService.js
-import axios from 'axios';
-
-const apiService = axios.create({
-  baseURL: "http://127.0.0.1:30017",
-});
-
-export default apiService;
